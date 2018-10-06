b7d4fbb1b32a8098968477aaad781344dbe55dbe
Esvin González <usac.ing.esvin@gmail.com>
2018-10-06 01:25:32 -0600

    Agregando script para crear catálogo de partidos políticos.

---
+++ database/V04_script_data_partidos.sql
@@ -0,0 +1,5 @@
+USE elecciones;
+INSERT INTO partido (id_partido, nombre, nombre_candidato) VALUES (1, "Partido 1", "Mario");
+INSERT INTO partido (id_partido, nombre, nombre_candidato) VALUES (2, "Partido 2", "Peach");
+INSERT INTO partido (id_partido, nombre, nombre_candidato) VALUES (3, "Partido 3", "Bowser");
+INSERT INTO partido (id_partido, nombre, nombre_candidato) VALUES (4, "Partido 4", "Waluigi");
